7c7d9af1496469dbd9ea94f1dbca0c101efee7b8
Teja Swaroop <tejaswaroop@Tejas-MacBook-Air.local>
2020-06-06 15:55:56 +0530

    first commit

---
+++ discord_webhook.py
@@ -0,0 +1,51 @@
+from discord_webhooks import DiscordWebhooks
+
+WEBHOOK_URL = 'https://discord.com/api/webhooks/718193141760524288/2xfij8tXfxJf6S4VVFaE9qoZob3s8Da2cSNzRuL99uPthRqtbXtLiZk70vAKwEvAnlPr'
+
+
+def send_msg(username, follower_change, followers, unfollowers,
+             followers_count, unfollowers_count, time):
+
+    if followers == [] and unfollowers == []:
+        print("No change in followers, so not sending message to discord")
+        return
+
+    if followers == []:
+        followers = 'None'
+
+    if unfollowers == []:
+        unfollowers = 'None'
+
+    webhook = DiscordWebhooks(WEBHOOK_URL)
+
+    webhook.set_content(title='Report for %s' % (time),
+                        description="Here's your report with :heart:")
+
+    # Attaches a footer
+    webhook.set_footer(text='-- Teja Swaroop')
+
+    # Appends a field
+    webhook.add_field(name='Username', value=username)
+    webhook.add_field(name='Total follower change', value=follower_change)
+
+    if unfollowers != 'None':
+        webhook.add_field(name='People who unfollowed you (%d)' %
+                          (unfollowers_count),
+                          value=','.join(unfollowers))
+    else:
+        webhook.add_field(name='People who unfollowed you (%d)' %
+                          (unfollowers_count),
+                          value=unfollowers)
+
+    if followers != 'None':
+        webhook.add_field(name='People who followed you (%d)' %
+                          (followers_count),
+                          value=','.join(followers))
+    else:
+        webhook.add_field(name='People who followed you (%d)' %
+                          (followers_count),
+                          value=followers)
+
+    webhook.send()
+
+    print("Sent message to discord")
---
+++ unfollow_check.py
@@ -0,0 +1,84 @@
+from igramscraper.instagram import Instagram
+from time import sleep
+from os import path
+import datetime
+import discord_webhook
+import ast
+import sys
+
+
+FOLLOWER_LIMIT = 10000000
+insta_username = 'teja.py'
+insta_password = '156@Swaroop'
+username = 'teja.techraj'
+MINS_TO_SLEEP = 55
+
+
+
+def check_unfollowers(current,old):
+	return list(set(old) - set(current))
+
+def check_followers(current,old):
+	return list(set(current) - set(old))
+
+
+while True:
+	try:
+		print("iter")
+		instagram = Instagram()
+		instagram.with_credentials(insta_username, insta_password)
+		instagram.login(force=False,two_step_verificator=True)
+		sleep(2) # Delay to mimic user
+
+		followers = []
+		account = instagram.get_account(username)
+		sleep(1)
+		curr_time = datetime.datetime.now()
+		curr_time = curr_time.strftime("%b %d, %Y - %H:%M:%S")
+		followers = instagram.get_followers(account.identifier, FOLLOWER_LIMIT, 100, delayed=True) # Get 150 followers of 'kevin', 100 a time with random delay between requests
+		# print(followers)
+
+		current_followers = []
+
+		for follower in followers['accounts']:
+			current_followers.append(follower.username)
+
+		del followers
+
+		if not path.exists("follower_list.txt"):
+			f = open("follower_list.txt","w")
+			f.write(str(current_followers))
+			f.close()
+		else:
+			f = open("follower_list.txt","r+")
+			old_followers = f.read()
+			f.close()
+			old_followers = ast.literal_eval(old_followers)
+
+			unfollowers = check_unfollowers(current_followers,old_followers)
+			followers = check_followers(current_followers,old_followers)
+
+			follower_change  = len(current_followers)-len(old_followers)
+
+			follow_count = len(followers)
+			unfollow_count = len(unfollowers)
+
+			discord_webhook.send_msg(username,follower_change,followers,unfollowers,follow_count,unfollow_count,curr_time)
+
+			f = open("follower_list.txt","w")
+			f.write(str(current_followers))
+			f.close()
+
+		sleep(MINS_TO_SLEEP*60)
+
+	except KeyboardInterrupt:
+		print("Exiting...")
+		sys.exit(0)
+	except:
+		continue
+
+
+
+
+
+
